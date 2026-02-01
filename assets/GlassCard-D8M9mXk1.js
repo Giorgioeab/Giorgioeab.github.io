a80075575e5fc4aba00565811331b712c32226d0
Giorgioeab <giorgioeabarbosa@gmail.com>
2026-02-01 01:35:47 -0300

    Updates

---
+++ assets/GlassCard-D8M9mXk1.js
@@ -0,0 +1,6 @@
+import{j as o,m as s}from"./index-BxMIlTW4.js";const t=({children:r,className:e="",hoverEffect:a=!1})=>o.jsx(s.div,{whileHover:a?{scale:1.02,y:-5}:{},className:`
+        backdrop-blur-md bg-white/10 dark:bg-black/30
+        border border-white/20 dark:border-white/10
+        shadow-xl rounded-2xl p-6
+        ${e}
+      `,children:r});export{t as G};
